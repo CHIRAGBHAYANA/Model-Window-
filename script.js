04b7fcbf479f90e9d0facc72810da7ed4960af16
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-10-14 11:48:44 +0530

    Adding logic in scriot file

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-'use strict';
+"use strict";
+const modal = document.querySelectorAll(".modal");
+
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelectorAll(".close-modal");
+
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = (i) => {};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  switch (i) {
+    case 0:
+      btnOpenModal[i].addEventListener("click", () => {
+        modal[i].classList.remove("hidden");
+        overlay.classList.remove("hidden");
+      });
+      btnCloseModal[i].addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      overlay.addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      break;
+    case 1:
+      btnOpenModal[i].addEventListener("click", () => {
+        modal[i].classList.remove("hidden");
+        overlay.classList.remove("hidden");
+      });
+      btnCloseModal[i].addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      overlay.addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      break;
+    case 2:
+      btnOpenModal[i].addEventListener("click", () => {
+        modal[i].classList.remove("hidden");
+        overlay.classList.remove("hidden");
+      });
+      btnCloseModal[i].addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      overlay.addEventListener("click", () => {
+        modal[i].classList.add("hidden");
+        overlay.classList.add("hidden");
+      });
+      break;
+  }
+}
