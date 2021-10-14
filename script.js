cb9907fb9f2789c925826bf4b128d4e8beb6e355
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-10-14 18:36:10 +0530

    Refactor the code

--- script.js
+++ script.js
@@ -8,50 +8,39 @@ const btnOpenModal = document.querySelectorAll(".show-modal");
 
 const closeModal = (i) => {};
 for (let i = 0; i < btnOpenModal.length; i++) {
+  const closeWindow = () => {
+    modal[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
   switch (i) {
     case 0:
       btnOpenModal[i].addEventListener("click", () => {
         modal[i].classList.remove("hidden");
         overlay.classList.remove("hidden");
       });
-      btnCloseModal[i].addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
-      overlay.addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
+      btnCloseModal[i].addEventListener("click", closeWindow);
+      overlay.addEventListener("click", closeWindow);
       break;
+
     case 1:
       btnOpenModal[i].addEventListener("click", () => {
         modal[i].classList.remove("hidden");
         overlay.classList.remove("hidden");
       });
-      btnCloseModal[i].addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
-      overlay.addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
+      btnCloseModal[i].addEventListener("click", closeWindow);
+      overlay.addEventListener("click", closeWindow);
       break;
+
     case 2:
       btnOpenModal[i].addEventListener("click", () => {
         modal[i].classList.remove("hidden");
         overlay.classList.remove("hidden");
       });
-      btnCloseModal[i].addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
-      overlay.addEventListener("click", () => {
-        modal[i].classList.add("hidden");
-        overlay.classList.add("hidden");
-      });
+      btnCloseModal[i].addEventListener("click", closeWindow);
+      overlay.addEventListener("click", closeWindow);
       break;
   }
+
   document.addEventListener("keyup", (e) => {
     if (e.key === "Escape" && !modal[i].classList.contains("hidden")) {
       modal[i].classList.add("hidden");
